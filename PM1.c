a8ce49b2e57bda2393ab7f689ab3d8ea1d82659a
Ravi Gupta <ravi720904@gmail.com>
2025-12-28 20:49:35 +0530

    Add files via upload

---
+++ PM1.c
@@ -0,0 +1,32 @@
+// #include <stdio.h>
+
+// int main(){
+//     printf("hello word");
+//     return 0;
+// }
+
+//******************NEXT****************
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("hello pw");
+//     printf("Hello hw");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("hello world\n hello pw");
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     printf("nn\n\nn\n");
+//     printf("n/n/nn/n");
+//     return 0;
+// }
+
